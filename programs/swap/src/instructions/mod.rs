95063b3b6fdbfec85df617954199114c2f6e9b2c
Nikhil chopra <nikhil0080012005@gmail.com>
2025-07-19 18:23:56 +0530

    added offer, logic for make_offer , structure for taking offer

--- programs/swap/src/instructions/mod.rs
+++ programs/swap/src/instructions/mod.rs
@@ -1,3 +1,8 @@
-pub mod initialize;
+pub mod shared;
+pub use shared::*;
 
-pub use initialize::*;
+pub mod make_offer;
+pub use make_offer::*;
+
+pub mod take_offer;
+pub use take_offer::*;
---
+++ programs/swap/src/instructions/take_offer.rs
@@ -0,0 +1,79 @@
+use anchor_lang::prelude::*;
+use anchor_spi::{
+    associated_token::AssociatedToken,
+    token_interface::{
+        close_account, transfer_checked, CloseAccount, Mint, TokenAccount, TokenInterface,
+        TrasferChecked,
+    },
+};
+
+use crate::Offer;
+
+use super::transfer_tokens;
+
+#[derive(Accounts)]
+pub struct TakeOffer<'info> {
+    #[account(mut)]
+    pub taker: Signer<'info>,
+
+    #[account(mut)]
+    pub maker: SystemAccount<'info>,
+
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
+    pub token_mint_b: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = token_mint_b,
+        associated_token:token_program = token_program,
+    )]
+    pub taker_token_account_a: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_b,
+        associated_token::authority = taker,
+        associated_token::token_program = token_program,
+    )]
+    pub taker_token_account_b: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        init_if_needed,
+        payer = taker,
+        associated_token::mint = token_mint_b,
+        associated_token::authority = maker,
+        associated_token::token_program = token_program
+    )]
+    pub maker_token_account_b: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    #[account(
+        mut,
+        close = maker,
+        has_one = maker,
+        has_one = token_mint_a,
+        has_one = token_mint_b,
+        seeds = [b"offer", maker.key().as_ref(), offer.id.to_le_bytes().as_ref()],
+        bump = offer.bump
+    )]
+    offer: Account<'info, Offer>,
+
+    #[account(
+        mut,
+        associated_token::mint = token_mint_a,
+        associated_token::authority = offer,
+        associated_token::token_program = token_program
+    )]
+    vault: InterfaceAccount<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>
+}
+
+pub fn send_wanted_tokens_to_maker(context: &Context<TakeOffer>) -> Result<()> {
+    transfer_tokens(
+        &context.accounts.taker_token
+    )
+}
